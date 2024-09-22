dc2df72b3ecc0f0f1a9502f67ba6946ef1b9f284
urimsu <urimsu56@gmail.com>
2024-09-23 00:17:34 +0200

    erster Build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CalculatorComponent } from "./calculator/calculator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
